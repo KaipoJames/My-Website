bc296be65b34fcc62fe8cc962e99224be266ab66
Kaipo <kaipojames12@gmail.com>
2021-07-09 15:56:41 -0700

    Major update to project page

--- javascript/projects.js
+++ javascript/projects.js
@@ -1,30 +1,23 @@
-import { variables } from "./variables.js";
+import { createText, removeText, projectsData } from "./project-data.js";
 
-const styleTextOnImageHover = () => {
-    const images = document.querySelectorAll(".project-img");
-    console.log(images);
-    addMouseEvents(images[0], "#ono-grindz-caption");
-    addMouseEvents(images[1], "#theory-site-caption");
-    addMouseEvents(images[2], "#calculator-caption");
-    addMouseEvents(images[3], "#vending-caption");
-}
-const addMouseEvents = (image, id) => {
-    const caption = document.querySelector(id);
-    image.addEventListener("mouseover", () => {
-        image.style.border = "2px solid #3c5df4";
-        caption.style.fontSize = "1.7vw";
-        caption.style.fontWeight = "bold";
-        caption.style.background = variables.blueGradient;
-        caption.style.color = "white";
-    });
-    image.addEventListener("mouseout", () => {
-        image.style.border = "1px solid black";
-        caption.style.fontSize = "1.5vw";
-        caption.style.fontWeight = "normal";
-        caption.style.background = variables.bgBlueGray;
-        caption.style.color = variables.textColorBlack;
-    })
-}
+const projects = document.querySelectorAll(".project");
+const projectImg = document.querySelectorAll(".project-img");
 
-styleTextOnImageHover();
+// Listen for project mouse events 
+(() => {
+    for (let i = 0; i < projects.length; i++) {
+        projects[i].addEventListener("mouseenter", () => {
+            const img = projectImg[i];
+            img.style.opacity = "0";
+
+            createText(projects[i], projectsData[i]);
+    
+        });
+        projects[i].addEventListener("mouseleave", () => {
+            const img = projectImg[i];
+            img.style.opacity = "1";
+            removeText(projects[i]);
+        });
+    }
+})();
 
